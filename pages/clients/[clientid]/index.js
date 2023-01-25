9360f1f62930706a071d734499ce3edb5497628e
Cailean <chorton44@aceproject.space>
2023-01-25 09:51:14 -0600

    Wrapped up
    Added final routing components and wrapped up the starter demo.

--- pages/clients/[clientid]/index.js
+++ pages/clients/[clientid]/index.js
@@ -1,12 +1,18 @@
 import { useRouter } from "next/router";
 
 function ClientProjects() {
-    const router = useRouer();
-    
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    //Load data...
+    router.push('/clients/cai/projecta');
+    // router.push({pathname: '/clients/[id]/[projectid]', query: {id: 'cai', projectid: 'projecta'}});
+  }
 
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
